9accfa0a6dcc401c678753fce120956d8612d5a3
Nagarjun k <53399371+nagarjunkumar@users.noreply.github.com>
2020-05-20 13:54:10 +0530

    first

---
+++ nextchar.py
@@ -0,0 +1,42 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.utils import np_utils
+import numpy as np
+
+np.random.seed(3)
+
+data="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+char_to_int = dict((c, i) for i, c in enumerate(data))
+
+int_to_char=dict((i,c)for i,c in enumerate(data))
+
+length=1
+X=[]
+Y=[]
+
+for i in range(0,len(data)-length)
+    inp=int_to_char[i]
+    oup=int_to_char[i+1]
+    X.append(char_to_int[inp])
+    Y.append(char_to_int[oup])
+
+X=np.reshape(X,(len(X),1,1))
+
+X=X/len(data)
+
+Y=np_utils.to_categorical(Y)
+
+model=Sequential()
+model.add(LSTM(32,input_shape=(1,1)))
+model.add(Dense(32,activation="relu"))
+model.add(Dense(26,activation="softmax"))
+model.compile(loss="categorical_crossentropy",optimizer="adam",metrics=['accuracy'])
+
+model.fit(X,Y,batch_size=len(X),verbose=1,epochs=100)
+
+pred=model.evaluate(X,Y)
+
+print("Accuracy is : ",pred[1]*100)
+
--- task3.py
+++
@@ -1,4 +0,0 @@
-def sum(a,b):
-    return a+b
-su=sum(2,3)
-print(su)
